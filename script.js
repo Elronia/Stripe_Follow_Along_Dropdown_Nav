cfd8bb495026e9b5a1f481e23d0fe98cd7e61e08
Elronia <zar.catherine@gmail.com>
2021-05-31 22:29:21 -0400

    figuring out dropdowns' height and width, where are located on the page using coordinates

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ const background = document.querySelector('.dropdownBackground');
 const nav = document.querySelector('.top');
 
 function handleEnter() {
-    console.log('ENTER!!!');
+    this.classList.add('trigger-enter');
+    setTimeout(() => this.classList.add('trigger-enter-active'), 150);
+    background.classList.add('open');
+
+    const dropdown = this.querySelector('.dropdown');
+    const dropdownCoords = dropdown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
+
+    const coords = {
+        height: dropdownCoords.height,
+        width: dropdownCoords.width
+    };
+    
 }
 
 function handleLeave() {
-    console.log('LEAVE!!');
+    this.classList.remove('trigger-enter', 'trigger-enter-active');
+    background.classList.remove('open');
 }
 
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
